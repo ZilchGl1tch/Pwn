49c6d997d2c51a8dfba9eba4482a034639e3525c
Srijiith <65584265+k1R4@users.noreply.github.com>
2021-05-14 14:52:50 +0530

    Create template.py

---
+++ template.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+from pwn import *
+
+exe = ELF("")
+libc = ELF("")
+context.binary = exe
+context.terminal = "kitty sh -c".split()
+context.log_level = "debug"
+IP, PORT = "", 0
+
+global io
+breakpoints = '''
+break main
+continue
+'''
+if len(sys.argv) > 1 and sys.argv[1] == "-r":
+    io = remote(IP, PORT)
+elif len(sys.argv) > 1 and sys.argv[1] == "-ng":
+    io = process(exe.path)
+else:
+    io = gdb.debug(exe.path, gdbscript=breakpoints)
+
+
+def s(a): return io.send(a)
+def sa(a, b): return io.sendafter(a, b)
+def sl(a): return io.sendline(a)
+def sla(a, b): return io.sendlineafter(a, b)
+def re(a): return io.recv(a)
+def reu(a): return io.recvuntil(a)
+def rl(): return io.recvline(False)
